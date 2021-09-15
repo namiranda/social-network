f44a2e2ffb460b3838651a425ec2ba0ddb19cd63
namiranda <naylenmiranda@gmail.com>
2021-09-14 21:51:43 -0300

    modifico endpoint en postlike, ni idea si es asi

--- src/main/java/io/redbee/socialnetwork/feeds/postLikes/PostLikeController.java
+++ src/main/java/io/redbee/socialnetwork/feeds/postLikes/PostLikeController.java
@@ -1,20 +1,25 @@
 package io.redbee.socialnetwork.feeds.postLikes;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/users/{userId}/posts/{postId}/likes")
 public class PostLikeController {
 
-    @Autowired
     PostLikeService postLikeService;
 
-    @PostMapping("/postlike")
-    void crearPostLike(@RequestBody PostLike postLike){
-        postLikeService.crearPostLike(postLike);
+    public PostLikeController(PostLikeService postLikeService) {
+        this.postLikeService = postLikeService;
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<PostLike> crearPostLike(@PathVariable Integer postId, @RequestHeader Integer userId){
+        PostLike newLike = new PostLike(postId, userId);
+        postLikeService.crearPostLike(newLike);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newLike);
     }
 
 }
